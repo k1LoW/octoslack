d24d59757f49990c4dc0b6619e092ec05b9ef1a9
k1LoW <k1lowxb@gmail.com>
2023-03-06 15:23:00 +0900

    Add test

--- transformer/func_test.go
+++ transformer/func_test.go
@@ -2,22 +2,81 @@ package transformer
 
 import (
 	"fmt"
+	"strings"
 	"testing"
 )
 
-func TestShortenLines(t *testing.T) {
+func TestQuote(t *testing.T) {
 	tests := []struct {
-		in   string
-		c    int
-		want string
+		in   []string
+		want []string
 	}{
-		{"a\nb\nc\n", 5, "a\nb\nc\n"},
-		{"a\nb\nc\n", 1, "a"},
-		{"a\nb\nc\n", 2, "a\nb"},
+		{
+			[]string{"a", "b", "c"},
+			[]string{"> a", "> b", "> c"},
+		},
+		{
+			[]string{"a", "", "c"},
+			[]string{"> a", ">", "> c"},
+		},
+		{
+			[]string{"following code", "``` sh", "go run main.go", "```", "ok"},
+			[]string{"> following code", "> ``` sh", "> go run main.go", "> ```", "> ok"},
+		},
 	}
 	for i, tt := range tests {
 		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
-			got := shortenLines(tt.in, tt.c, "")
+			got := quote(strings.Join(tt.in, "\n"))
+			if got != strings.Join(tt.want, "\n") {
+				t.Errorf("got %#v\nwant %#v", got, strings.Join(tt.want, "\n"))
+			}
+		})
+	}
+}
+
+func TestQuoteMarkdown(t *testing.T) {
+	tests := []struct {
+		in   []string
+		want []string
+	}{
+		{
+			[]string{"a", "b", "c"},
+			[]string{"> a", "> b", "> c"},
+		},
+		{
+			[]string{"a", "", "c"},
+			[]string{"> a", ">", "> c"},
+		},
+		{
+			[]string{"following code", "``` sh", "go run main.go", "```", "ok"},
+			[]string{"> following code", "> ``` sh", "go run main.go", "```", "> ok"},
+		},
+	}
+	for i, tt := range tests {
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			got := quoteMarkdown(strings.Join(tt.in, "\n"))
+			if got != strings.Join(tt.want, "\n") {
+				t.Errorf("got %#v\nwant %#v", got, strings.Join(tt.want, "\n"))
+			}
+		})
+	}
+}
+
+func TestShortenLines(t *testing.T) {
+	tests := []struct {
+		in      string
+		message string
+		c       int
+		want    string
+	}{
+		{"a\nb\nc\n", "", 5, "a\nb\nc\n"},
+		{"a\nb\nc\n", "", 1, "a"},
+		{"a\nb\nc\n", "", 2, "a\nb"},
+		{"a\nb\nc\n", "(snip)", 2, "a\nb\n(snip)"},
+	}
+	for i, tt := range tests {
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			got := shortenLines(tt.in, tt.c, tt.message)
 			if got != tt.want {
 				t.Errorf("got %#v\nwant %#v", got, tt.want)
 			}
